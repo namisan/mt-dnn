f2329f584d4f063dfaaf78bd8dd83eb01e962b16
Xiaodong Liu <liuxiaodongby@gmail.com>
2019-08-08 16:14:11 -0700

    superglue

---
+++ experiments/superglue/prepro.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+python experiments/glue/glue_prepro.py
+python prepro_std.py --model bert-base-uncased --root_dir data/canonical_data --task_def experiments/glue/glue_task_def.yml --do_lower_case $1
