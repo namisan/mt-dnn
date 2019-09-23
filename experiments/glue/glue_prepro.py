df1b5f0eaf5f3f9ba0fb588fc4b6461034696cbe
Yu Wang <anselmwang@gmail.com>
2019-09-23 15:21:13 -0700

    add MRC to BatchGen & fix unit test

--- experiments/glue/glue_prepro.py
+++ experiments/glue/glue_prepro.py
@@ -3,9 +3,9 @@ import argparse
 import random
 from sys import path
 
+path.append(os.getcwd())
 from experiments.common_utils import dump_rows
 
-path.append(os.getcwd())
 from data_utils.log_wrapper import create_logger
 from experiments.glue.glue_utils import *
 
