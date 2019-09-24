bd9bc21a9d4f36f69cbf4c9f473e5026b70a3385
Xiaodong Liu <liuxiaodongby@gmail.com>
2019-09-24 15:27:08 -0700

    support roberta prediction

--- predict.py
+++ predict.py
@@ -4,14 +4,36 @@ import os
 import torch
 
 from data_utils.task_def import TaskType
-from experiments.exp_def import TaskDefs
-from experiments.glue.glue_utils import eval_model
+from experiments.exp_def import TaskDefs, EncoderModelType
+#from experiments.glue.glue_utils import eval_model
+
 from mt_dnn.batcher import BatchGen
 from mt_dnn.model import MTDNNModel
+from data_utils.metrics import calc_metrics
 
 def dump(path, data):
     with open(path, 'w') as f:
         json.dump(data, f)
+
+def eval_model(model, data, metric_meta, use_cuda=True, with_label=True):
+    data.reset()
+    if use_cuda:
+        model.cuda()
+    predictions = []
+    golds = []
+    scores = []
+    ids = []
+    metrics = {}
+    for batch_meta, batch_data in data:
+        score, pred, gold = model.predict(batch_meta, batch_data)
+        predictions.extend(pred)
+        golds.extend(gold)
+        scores.extend(score)
+        ids.extend(batch_meta['uids'])
+    if with_label:
+        metrics = calc_metrics(metric_meta, golds, predictions, scores, ids)
+    return metrics, predictions, scores, golds, ids
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--task_def", type=str, default="experiments/glue/glue_task_def.yml")
@@ -43,16 +65,6 @@ pw_task = False
 if task_type == TaskType.Ranking:
     pw_task = True
 
-# load data
-test_data = BatchGen(BatchGen.load(args.prep_input, False, pairwise=pw_task, maxlen=args.max_seq_len),
-                     batch_size=args.batch_size_eval,
-                     gpu=args.cuda, is_train=False,
-                     task_id=args.task_id,
-                     maxlen=args.max_seq_len,
-                     pairwise=pw_task,
-                     data_type=data_type,
-                     task_type=task_type)
-
 # load model
 checkpoint_path = args.checkpoint
 assert os.path.exists(checkpoint_path)
@@ -63,12 +75,26 @@ else:
 config = state_dict['config']
 config["cuda"] = args.cuda
 model = MTDNNModel(config, state_dict=state_dict)
+model.load(checkpoint_path)
+encoder_type = config.get('encoder_type', EncoderModelType.BERT)
+# load data
+test_data = BatchGen(BatchGen.load(args.prep_input, False, pairwise=pw_task, maxlen=args.max_seq_len),
+                     batch_size=args.batch_size_eval,
+                     gpu=args.cuda, is_train=False,
+                     task_id=args.task_id,
+                     maxlen=args.max_seq_len,
+                     pairwise=pw_task,
+                     data_type=data_type,
+                     task_type=task_type,
+                     encoder_type=encoder_type)
+
+
 with torch.no_grad():
     test_metrics, test_predictions, scores, golds, test_ids = eval_model(model, test_data,
-                                                                     metric_meta=metric_meta,
-                                                                     use_cuda=args.cuda, with_label=args.with_label)
+                                                                         metric_meta=metric_meta,
+                                                                         use_cuda=args.cuda, with_label=args.with_label)
 
-results = {'metrics': test_metrics, 'predictions': test_predictions, 'uids': test_ids, 'scores': scores}
-dump(args.score, results)
-if args.with_label:
-    print(test_metrics)
+    results = {'metrics': test_metrics, 'predictions': test_predictions, 'uids': test_ids, 'scores': scores}
+    dump(args.score, results)
+    if args.with_label:
+        print(test_metrics)
