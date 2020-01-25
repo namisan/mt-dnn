8e32276daa6eaf1941e8d49196212ee6f7a33715
Yu Wang <yuwan@microsoft.com>
2020-01-25 05:27:59 +0000

    add test for prepro_std.py

---
+++ int_test.py
@@ -0,0 +1,35 @@
+import os
+import os.path as path
+import filecmp
+import subprocess
+import glob
+
+def assert_dir_equal(dir0, dir1):
+    for file_path0 in glob.glob(path.join(dir0, "*")):
+        file_name = path.split(file_path0)[1]
+        file_path1 = path.join(dir1, file_name)
+        assert filecmp.cmp(file_path0, file_path1, shallow=False), \
+               '%s diff in two directories "%s" and "%s"' % (file_name, dir0, dir1)
+
+def test_prepro_std(cmd, src_dir, task_def_path, target_dir, expected_dir):
+    subprocess.call("rm -rf %s" % target_dir, shell=True)
+    assert not os.access(target_dir, os.F_OK), "preprocessed target directory already exist"
+    subprocess.call(BERT_CMD % (src_dir, task_def_path), shell=True, stdout=subprocess.DEVNULL)
+    assert_dir_equal(target_dir, expected_dir)
+
+BERT_CMD = "python prepro_std.py --model bert-base-uncased --root_dir %s --task_def %s --do_lower_case "
+ROBERTA_CMD = "python prepro_std.py --model roberta-base --roberta_path mt_dnn_models/roberta --root_dir %s --task_def %s --do_lower_case "
+
+test_prepro_std(BERT_CMD,
+                "test/glue/glue_data",
+                "test/glue/glue_task_def.yml",
+                "test/glue/glue_data/bert_uncased_lower",
+                "test/glue/expected/bert_uncased_lower"
+                )
+
+test_prepro_std(BERT_CMD,
+                "test/glue/glue_data",
+                "test/glue/glue_task_def.yml",
+                "test/glue/glue_data/roberta_cased_lower",
+                "test/glue/expected/roberta_cased_lower"
+                )
