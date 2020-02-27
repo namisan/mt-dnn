d2caafc42ba1e52554f5be8d18eec5bbf8aa9baf
Xiaodong Liu <liuxiaodongby@gmail.com>
2020-02-26 21:47:17 -0800

    fix predict file

--- predict.py
+++ predict.py
@@ -3,9 +3,9 @@ import json
 import os
 import torch
 
+from data_utils.task_def import TaskType
 from experiments.exp_def import TaskDefs, EncoderModelType
-#from experiments.glue.glue_utils import eval_model
-
+from torch.utils.data import Dataset, DataLoader, BatchSampler
 from mt_dnn.batcher import SingleTaskDataset, Collater
 from mt_dnn.model import MTDNNModel
 from data_utils.metrics import calc_metrics
@@ -35,12 +35,16 @@ parser.add_argument("--checkpoint", default='mt_dnn_models/bert_model_base_uncas
 args = parser.parse_args()
 
 # load task info
+task = args.task
 task_defs = TaskDefs(args.task_def)
-task_def = task_defs.get_task_def(args.task)
-data_type = task_def.data_type
-task_type = task_def.task_type
-metric_meta = task_def.metric_meta
-
+assert args.task in task_defs._task_type_map
+assert args.task in task_defs._data_type_map
+assert args.task in task_defs._metric_meta_map
+prefix = task.split('_')[0]
+task_def = task_defs.get_task_def(prefix)
+data_type = task_defs._data_type_map[args.task]
+task_type = task_defs._task_type_map[args.task]
+metric_meta = task_defs._metric_meta_map[args.task]
 # load model
 checkpoint_path = args.checkpoint
 assert os.path.exists(checkpoint_path)
@@ -54,9 +58,8 @@ model = MTDNNModel(config, state_dict=state_dict)
 model.load(checkpoint_path)
 encoder_type = config.get('encoder_type', EncoderModelType.BERT)
 # load data
-test_data_set = SingleTaskDataset(args.prep_input, False, task_def=task_def, maxlen=args.max_seq_len)
-collater = Collater(gpu=args.cuda, is_train=False, task_id=args.task_id, task_type=task_type,
-                    data_type=data_type, encoder_type=encoder_type)
+test_data_set = SingleTaskDataset(args.prep_input, False, maxlen=args.max_seq_len, task_id=args.task_id, task_def=task_def)
+collater = Collater(is_train=False, encoder_type=encoder_type)
 test_data = DataLoader(test_data_set, batch_size=args.batch_size_eval, collate_fn=collater.collate_fn, pin_memory=args.cuda)
 
 with torch.no_grad():
