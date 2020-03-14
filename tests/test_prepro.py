5bcc59335ccce155cb552d862f6ef9fef149b300
Xiaodong Liu <liuxiaodongby@gmail.com>
2020-03-14 09:38:10 -0700

    fix test sample

--- tests/test_prepro.py
+++ tests/test_prepro.py
@@ -37,6 +37,7 @@ def are_dir_trees_equal(dir1, dir2):
    """
 
     dirs_cmp = filecmp.dircmp(dir1, dir2)
+    
     if len(dirs_cmp.left_only)>0 or len(dirs_cmp.right_only)>0 or \
         len(dirs_cmp.funny_files)>0:
         return False
@@ -58,5 +59,5 @@ def test_prepro():
 
     result = subprocess.check_output("python experiments/glue/glue_prepro.py --root_dir run_test/sample_data", stderr=subprocess.STDOUT, shell=True)
     result = subprocess.check_output("python prepro_std.py --model bert-base-uncased --root_dir run_test/sample_data/canonical_data --task_def experiments/glue/glue_task_def.yml --do_lower_case", stderr=subprocess.STDOUT, shell=True)
-    assert are_dir_trees_equal("./run_test/sample_data/canonical_data/bert_uncased_lower", "./sample_data/output")
+    assert are_dir_trees_equal("./run_test/sample_data/canonical_data/bert_base_uncased_lower", "./sample_data/output")
 
