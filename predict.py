ed5ebac8565c19360164f1ecd70adf07f9bbe1f0
Xiaodong Liu <xiaodl@microsoft.com>
2022-01-23 13:25:56 -0800

    update predict

--- predict.py
+++ predict.py
@@ -1,4 +1,5 @@
 import argparse
+from ast import arg
 import json
 import os
 import torch
@@ -11,6 +12,7 @@ from mt_dnn.batcher import SingleTaskDataset, Collater
 from mt_dnn.model import MTDNNModel
 from data_utils.metrics import calc_metrics
 from mt_dnn.inference import eval_model
+from data_utils.metrics import Metric
 
 def dump(path, data):
     with open(path, 'w') as f:
@@ -24,7 +26,7 @@ parser.add_argument("--task_id", type=int, help="the id of this task when traini
 parser.add_argument("--prep_input", type=str)
 parser.add_argument("--with_label", action="store_true")
 parser.add_argument("--score", type=str, help="score output path")
-
+parser.add_argument("--metric", type=str, default=None)
 parser.add_argument('--max_seq_len', type=int, default=512)
 parser.add_argument('--batch_size_eval', type=int, default=8)
 parser.add_argument('--cuda', type=bool, default=torch.cuda.is_available(),
@@ -45,13 +47,20 @@ task_def = task_defs.get_task_def(prefix)
 data_type = task_defs._data_type_map[args.task]
 task_type = task_defs._task_type_map[args.task]
 metric_meta = task_defs._metric_meta_map[args.task]
+
+if args.metric:
+    metric_meta = [Metric[args.metric]]
 # load model
 checkpoint_path = args.checkpoint
 assert os.path.exists(checkpoint_path)
+
 if args.cuda:
-    state_dict = torch.load(checkpoint_path)
+    device = torch.device("cuda")
 else:
-    state_dict = torch.load(checkpoint_path, map_location="cpu")
+    device = torch.device("cpu")
+
+state_dict = torch.load(checkpoint_path, map_location=device)
+
 config = state_dict['config']
 config["cuda"] = args.cuda
 task_def = task_defs.get_task_def(prefix)
@@ -62,17 +71,17 @@ config['fp16'] = False
 config['answer_opt'] = 0
 config['adv_train'] = False
 del state_dict['optimizer']
-model = MTDNNModel(config, state_dict=state_dict)
+
+model = MTDNNModel(config, device=device, state_dict=state_dict)
 encoder_type = config.get('encoder_type', EncoderModelType.BERT)
 # load data
 test_data_set = SingleTaskDataset(args.prep_input, False, maxlen=args.max_seq_len, task_id=args.task_id, task_def=task_def)
 collater = Collater(is_train=False, encoder_type=encoder_type)
 test_data = DataLoader(test_data_set, batch_size=args.batch_size_eval, collate_fn=collater.collate_fn, pin_memory=args.cuda)
-
 with torch.no_grad():
     test_metrics, test_predictions, scores, golds, test_ids = eval_model(model, test_data,
                                                                          metric_meta=metric_meta,
-                                                                         use_cuda=args.cuda, with_label=args.with_label)
+                                                                         device=device, with_label=args.with_label)
 
     results = {'metrics': test_metrics, 'predictions': test_predictions, 'uids': test_ids, 'scores': scores}
     dump(args.score, results)
