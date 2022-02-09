12634522f3aed47c7d150b4b4d2cf56ae11dcd2c
Xiaodong Liu <xiaodl@microsoft.com>
2022-02-08 23:01:39 -0800

    update

--- download.sh
+++ download.sh
@@ -9,11 +9,6 @@ if [ ! -d ${BERT_DIR}  ]; then
   mkdir ${BERT_DIR}
 fi
 
-## Download bert models
-wget https://mrc.blob.core.windows.net/mt-dnn-model/bert_model_base_v2.pt -O "${BERT_DIR}/bert_model_base_uncased.pt"
-wget https://mrc.blob.core.windows.net/mt-dnn-model/bert_model_large_v2.pt -O "${BERT_DIR}/bert_model_large_uncased.pt"
-wget https://mrc.blob.core.windows.net/mt-dnn-model/bert_base_chinese.pt -O "${BERT_DIR}/bert_model_base_chinese.pt"
-
 ## Download MT-DNN models
 wget https://mrc.blob.core.windows.net/mt-dnn-model/mt_dnn_base.pt -O "${BERT_DIR}/mt_dnn_base_uncased.pt"
 wget https://mrc.blob.core.windows.net/mt-dnn-model/mt_dnn_large.pt -O "${BERT_DIR}/mt_dnn_large_uncased.pt"
@@ -21,21 +16,6 @@ wget https://mrc.blob.core.windows.net/mt-dnn-model/mt_dnn_large.pt -O "${BERT_D
 ## MT-DNN-KD
 wget https://mrc.blob.core.windows.net/mt-dnn-model/mt_dnn_kd_large_cased.pt -O "${BERT_DIR}/mt_dnn_kd_large_cased.pt"
 
-
-## download ROBERTA
-wget https://dl.fbaipublicfiles.com/fairseq/models/roberta.base.tar.gz -O "roberta.base.tar.gz"
-wget https://dl.fbaipublicfiles.com/fairseq/models/roberta.large.tar.gz -O "roberta.large.tar.gz"
-tar xvf roberta.base.tar.gz
-mv "roberta.base" "${BERT_DIR}/"
-tar xvf roberta.large.tar.gz
-mv "roberta.large" "${BERT_DIR}/"
-rm "roberta.base.tar.gz"
-rm "roberta.large.tar.gz"
-
-mkdir "${BERT_DIR}/roberta"
-wget -N 'https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/encoder.json' -O "${BERT_DIR}/roberta/encoder.json"
-wget -N 'https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/vocab.bpe' -O "${BERT_DIR}/roberta/vocab.bpe"
-wget -N 'https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/dict.txt' -O "${BERT_DIR}/roberta/ict.txt"
 
 if [ "$1" == "model_only" ]; then
   exit 1
