9b301dcc55a6ef4f95cb60ea88e3ae2dccaf06b3
Xiaodong Liu <xiaodl@microsoft.com>
2022-02-25 13:33:20 -0800

    fix glue ft script

--- experiments/glue/run_glue_finetuning.sh
+++ experiments/glue/run_glue_finetuning.sh
@@ -31,7 +31,7 @@ echo "GPU counts: ${num_gpus}"
 export ROOT_DIR="glue_app"
 export EPOCH=3
 export LR="5e-5"
-export OPTIM="adamax"
+export OPTIM="adamw"
 export TASK_DEF="experiments/glue/glue_task_def.yml"
 export BS=${batch_size}
 
@@ -76,7 +76,7 @@ elif [ ${model_type} == "electra" ]; then
   MD="google/electra-${model_size}-discriminator"
   DD="bert-base-uncased"
   ED=7
-if [ ${model_type} == "mtdnn" ]; then
+elif [ ${model_type} == "mtdnn" ]; then
   MD="mt_dnn_modes/mt_dnn_${model_size}_uncased.pt"
   DD="bert-${model_size}-uncased"
   ED=1
@@ -91,8 +91,8 @@ echo $output_dir
 mkdir -p ${output_dir}
 
 if [[ -f "${output_dir}/model*.pt" ]]; then
-  rm "${output_dir}/model*.pt"
-  rm "${output_dir}/config.json"
+ rm "${output_dir}/model*.pt"
+ rm "${output_dir}/config.json"
 fi
 
 echo "Training ${task} tokenized by ${DD} with ${MD}"
@@ -100,16 +100,16 @@ echo "Training ${task} tokenized by ${DD} with ${MD}"
 LOG_FILE="${output_dir}/mt-dnn-train.log"
 #
 if [ ${num_gpus} -ge 2 ]; then
-  # multi gpu training
-  # DDP config
-  export MASTER_ADDR=localhost
-  export MASTER_PORT="8787"
-  export NNODES=1
-  export NODE_RANK=0
-  export GPUS_PER_NODE=${num_gpus}
-  export WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
-  export DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
-  python -m torch.distributed.launch $DISTRIBUTED_ARGS train.py --data_dir=${data_dir}/${DD} --task_def=${TASK_DEF}  --train_dataset=${train_dataset} --test_dataset=${test_dataset} --init_checkpoint=${MD} --batch_size=${BS} --learning_rate=${LR} --epochs=${EPOCH} --encoder_type=${ED} --optimizer=${OPTIM} --output_dir=${output_dir} --log_file=${LOG_FILE}
+ # multi gpu training
+ # DDP config
+ export MASTER_ADDR=localhost
+ export MASTER_PORT="8787"
+ export NNODES=1
+ export NODE_RANK=0
+ export GPUS_PER_NODE=${num_gpus}
+ export WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+ export DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
+ python -m torch.distributed.launch $DISTRIBUTED_ARGS train.py --data_dir=${data_dir}/${DD} --task_def=${TASK_DEF}  --train_dataset=${train_dataset} --test_dataset=${test_dataset} --init_checkpoint=${MD} --batch_size=${BS} --learning_rate=${LR} --epochs=${EPOCH} --encoder_type=${ED} --optimizer=${OPTIM} --output_dir=${output_dir} --log_file=${LOG_FILE}
 else
-  python train.py --data_dir=${data_dir}/${DD} --task_def=${TASK_DEF} --train_dataset=${train_dataset} --test_dataset=${test_dataset} --init_checkpoint=${MD} --batch_size=${BS} --learning_rate=${LR} --epochs=${EPOCH} --encoder_type=${ED} --optimizer=${OPTIM} --output_dir=${output_dir} --log_file=${LOG_FILE}
+ python train.py --data_dir=${data_dir}/${DD} --task_def=${TASK_DEF} --train_dataset=${train_dataset} --test_dataset=${test_dataset} --init_checkpoint=${MD} --batch_size=${BS} --learning_rate=${LR} --epochs=${EPOCH} --encoder_type=${ED} --optimizer=${OPTIM} --output_dir=${output_dir} --log_file=${LOG_FILE}
 fi
