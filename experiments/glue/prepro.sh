a78a86651dde3e0cce6fd0f091993c8d4a86a36e
Xiaodong Liu <xiaodl@microsoft.com>
2022-03-11 08:45:04 -0800

    update glue prepro

--- experiments/glue/prepro.sh
+++ experiments/glue/prepro.sh
@@ -1,3 +1,21 @@
-#! /bin/sh
+#!/usr/bin/env bash
+
+############################### 
+# Data prepro pipeline for MT-DNN.
+# By xiaodong
+############################### 
+
+## dump original data into tsv
 python experiments/glue/glue_prepro.py
-python prepro_std.py --model bert-base-uncased --root_dir data/canonical_data --task_def experiments/glue/glue_task_def.yml --workers 32
+
+
+declare -a PLMS=('bert-base-uncased' 'roberta-base' 'microsoft/deberta-base' 't5-base')
+
+# prepo GLUE data for all PLMs.
+for plm in "${PLMS[@]}"
+do
+  echo "Prepro GLUE data for $plm"
+  python prepro_std.py --model $plm --root_dir data/canonical_data --task_def experiments/glue/glue_task_def.yml --workers 32
+done
+
+
